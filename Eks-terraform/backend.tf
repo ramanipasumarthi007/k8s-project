4457814d969b95c68cfcdb4d07e98d907d797965
ramanipasumarthi007 <ramani.pasumarthi007@gmail.com>
2026-07-12 17:06:11 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mustafa.devops77" # Replace with your actual S3 bucket name
+    bucket = "ravitest5678" # Replace with your actual S3 bucket name
     key    = "Prod/terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
   }
 }
